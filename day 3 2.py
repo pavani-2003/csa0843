356349a5f247375a8710562aea0d88234af4e1b2
pavani-2003 <112242637+pavani-2003@users.noreply.github.com>
2022-09-29 09:04:09 +0530

    Add files via upload

---
+++ day 3 2.py
@@ -0,0 +1,16 @@
+n=int(input("Enter the number of elements="))
+arr=[]
+for i in range(0,n):
+    a=int(input("Enter the elements="))
+    arr.append(a)
+even=0
+odd=0
+count=0
+if(n<=0):
+    print("INVALID ENTRY")
+    exit()
+for i in range(0,n):
+    if(arr[i]%2==0):
+        even=even+(arr[i]*arr[i])
+    else:
+        odd=odd+(arr[i]*arr[i])
