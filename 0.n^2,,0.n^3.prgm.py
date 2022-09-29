9a3cf0e5a0689cc73c7cbc8a8d8716725be0292e
pavani-2003 <112242637+pavani-2003@users.noreply.github.com>
2022-09-29 11:24:31 +0530

    Add files via upload

---
+++ 0.n^2,,0.n^3.prgm.py
@@ -0,0 +1,8 @@
+n=float(input("enter the number"))
+square=n*n
+cube=n*n*n
+if(n<=0):
+    print("invalid input")
+else:
+    print("tne square of given number is:",square)
+    print("the cube of given number is:",cube)
